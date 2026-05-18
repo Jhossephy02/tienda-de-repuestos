261524ed7359d030443fa5f185e0172f634b3d6b
jhossephy <Jhossephy02@users.noreply.github.com>
2026-05-17 23:06:59 -0500

    update images in seeds.sql

--- model/seeds.sql
+++ model/seeds.sql
@@ -11,36 +11,36 @@ INSERT INTO marcas (nombre) VALUES
 -- Insertar Productos (Ampliación)
 INSERT INTO productos (codigo, nombre, categoria_id, marca_id, precio_costo, precio_venta, stock, stock_minimo, imagen_url) VALUES
 -- Motor
-('MOT-001', 'Pistón Estándar Honda CB190R', 1, 1, 120.00, 180.00, 15, 5, 'https://m.media-amazon.com/images/I/51V66Z4C6RL._AC_SL1000_.jpg'),
-('MOT-002', 'Válvulas de Admisión Yamaha FZ25', 1, 2, 45.00, 75.00, 20, 4, 'https://m.media-amazon.com/images/I/61K8P1XN1DL._AC_SL1500_.jpg'),
-('MOT-003', 'Empaque de Culata Pulsar NS200', 1, 3, 15.00, 35.00, 40, 10, 'https://m.media-amazon.com/images/I/71Nn0O1WpDL._AC_SL1500_.jpg'),
+('MOT-001', 'Pistón Estándar Honda CB190R', 1, 1, 120.00, 180.00, 15, 5, 'https://images.unsplash.com/photo-1590623253549-368c70d42eb1?q=80&w=2070&auto=format&fit=crop'),
+('MOT-002', 'Válvulas de Admisión Yamaha FZ25', 1, 2, 45.00, 75.00, 20, 4, 'https://images.unsplash.com/photo-1632360742111-9e73d32840d2?q=80&w=2070&auto=format&fit=crop'),
+('MOT-003', 'Empaque de Culata Pulsar NS200', 1, 3, 15.00, 35.00, 40, 10, 'https://images.unsplash.com/photo-1620241416843-0570a256a5c2?q=80&w=2070&auto=format&fit=crop'),
 
 -- Frenos
-('FRE-001', 'Pastillas de Freno Delanteras NS200', 2, 3, 25.00, 45.00, 50, 10, 'https://m.media-amazon.com/images/I/71H5zN0vP9L._AC_SL1500_.jpg'),
-('FRE-002', 'Disco de Freno Delantero Yamaha R3', 2, 2, 85.00, 150.00, 12, 3, 'https://m.media-amazon.com/images/I/71Y+S0D0uYL._AC_SL1500_.jpg'),
-('FRE-003', 'Zapatas de Freno Trasero Torito Bajaj', 2, 3, 18.00, 32.00, 60, 15, 'https://m.media-amazon.com/images/I/61Z6v+I+KGL._AC_SL1200_.jpg'),
+('FRE-001', 'Pastillas de Freno Delanteras NS200', 2, 3, 25.00, 45.00, 50, 10, 'https://images.unsplash.com/photo-1579581897451-c0326e632833?q=80&w=2070&auto=format&fit=crop'),
+('FRE-002', 'Disco de Freno Delantero Yamaha R3', 2, 2, 85.00, 150.00, 12, 3, 'https://images.unsplash.com/photo-1579581897451-c0326e632833?q=80&w=2070&auto=format&fit=crop'),
+('FRE-003', 'Zapatas de Freno Trasero Torito Bajaj', 2, 3, 18.00, 32.00, 60, 15, 'https://images.unsplash.com/photo-1579581897451-c0326e632833?q=80&w=2070&auto=format&fit=crop'),
 
 -- Transmisión
-('TRA-001', 'Kit de Arrastre Honda CB190R', 3, 1, 110.00, 180.00, 15, 5, 'https://m.media-amazon.com/images/I/81hU1-0mOLL._AC_SL1500_.jpg'),
-('TRA-002', 'Cadena de Tracción 428H-132L', 3, 3, 45.00, 75.00, 25, 8, 'https://m.media-amazon.com/images/I/71hO0K0vP9L._AC_SL1500_.jpg'),
+('TRA-001', 'Kit de Arrastre Honda CB190R', 3, 1, 110.00, 180.00, 15, 5, 'https://images.unsplash.com/photo-1635324681604-51e9411933e1?q=80&w=2070&auto=format&fit=crop'),
+('TRA-002', 'Cadena de Tracción 428H-132L', 3, 3, 45.00, 75.00, 25, 8, 'https://images.unsplash.com/photo-1635324681604-51e9411933e1?q=80&w=2070&auto=format&fit=crop'),
 
 -- Eléctrico
-('ELE-001', 'Batería Gel 12V 7Ah Bosch', 5, 8, 85.00, 145.00, 20, 5, 'https://m.media-amazon.com/images/I/61Y0+S0D0uL._AC_SL1500_.jpg'),
-('ELE-002', 'Foco LED H4 Alta Potencia', 5, 10, 25.00, 55.00, 40, 10, 'https://m.media-amazon.com/images/I/61I0+S0D0uL._AC_SL1500_.jpg'),
-('ELE-003', 'Bujía Iridium NGK CPR8EAIX-9', 5, 8, 30.00, 55.00, 100, 20, 'https://m.media-amazon.com/images/I/61mD+I2U0sL._AC_SL1000_.jpg'),
+('ELE-001', 'Batería Gel 12V 7Ah Bosch', 5, 8, 85.00, 145.00, 20, 5, 'https://images.unsplash.com/photo-1620241416843-0570a256a5c2?q=80&w=2070&auto=format&fit=crop'),
+('ELE-002', 'Foco LED H4 Alta Potencia', 5, 10, 25.00, 55.00, 40, 10, 'https://images.unsplash.com/photo-1605333146056-b0972b217118?q=80&w=2070&auto=format&fit=crop'),
+('ELE-003', 'Bujía Iridium NGK CPR8EAIX-9', 5, 8, 30.00, 55.00, 100, 20, 'https://images.unsplash.com/photo-1632360742111-9e73d32840d2?q=80&w=2070&auto=format&fit=crop'),
 
 -- Suspensión
-('SUS-001', 'Amortiguador Trasero Pulsar 180', 4, 3, 120.00, 210.00, 10, 3, 'https://m.media-amazon.com/images/I/61W0+S0D0uL._AC_SL1500_.jpg'),
-('SUS-002', 'Retenes de Telescópica Yamaha FZ', 4, 2, 15.00, 35.00, 30, 8, 'https://m.media-amazon.com/images/I/51I0+S0D0uL._AC_SL1000_.jpg'),
+('SUS-001', 'Amortiguador Trasero Pulsar 180', 4, 3, 120.00, 210.00, 10, 3, 'https://images.unsplash.com/photo-1635324681604-51e9411933e1?q=80&w=2070&auto=format&fit=crop'),
+('SUS-002', 'Retenes de Telescópica Yamaha FZ', 4, 2, 15.00, 35.00, 30, 8, 'https://images.unsplash.com/photo-1620241416843-0570a256a5c2?q=80&w=2070&auto=format&fit=crop'),
 
 -- Accesorios
-('ACC-001', 'Casco Integral LS2 Rapid', 8, 10, 180.00, 280.00, 8, 2, 'https://m.media-amazon.com/images/I/61S0+S0D0uL._AC_SL1500_.jpg'),
-('ACC-002', 'Guantes de Protección Fox Racing', 8, 10, 35.00, 65.00, 25, 5, 'https://m.media-amazon.com/images/I/71P0+S0D0uL._AC_SL1500_.jpg'),
-('ACC-003', 'Espejos Deportivos Universales', 8, 1, 18.00, 45.00, 20, 5, 'https://m.media-amazon.com/images/I/61Q0+S0D0uL._AC_SL1500_.jpg'),
+('ACC-001', 'Casco Integral LS2 Rapid', 8, 10, 180.00, 280.00, 8, 2, 'https://images.unsplash.com/photo-1558980394-0a37b3693671?q=80&w=2070&auto=format&fit=crop'),
+('ACC-002', 'Guantes de Protección Fox Racing', 8, 10, 35.00, 65.00, 25, 5, 'https://images.unsplash.com/photo-1518151240078-4357321eb93a?q=80&w=2070&auto=format&fit=crop'),
+('ACC-003', 'Espejos Deportivos Universales', 8, 1, 18.00, 45.00, 20, 5, 'https://images.unsplash.com/photo-1558980664-769d59546b3d?q=80&w=2070&auto=format&fit=crop'),
 
 -- Lubricantes
-('LUB-001', 'Aceite Castrol Power1 10W40 4T', 6, 6, 22.00, 38.00, 100, 20, 'https://m.media-amazon.com/images/I/61Y0+S0D0uL._AC_SL1500_.jpg'),
-('LUB-002', 'Aceite Motul 7100 20W50 Sintético', 6, 7, 45.00, 75.00, 40, 10, 'https://m.media-amazon.com/images/I/61X0+S0D0uL._AC_SL1500_.jpg');
+('LUB-001', 'Aceite Castrol Power1 10W40 4T', 6, 6, 22.00, 38.00, 100, 20, 'https://images.unsplash.com/photo-1610486333908-1111624b52c0?q=80&w=2070&auto=format&fit=crop'),
+('LUB-002', 'Aceite Motul 7100 20W50 Sintético', 6, 7, 45.00, 75.00, 40, 10, 'https://images.unsplash.com/photo-1610486333908-1111624b52c0?q=80&w=2070&auto=format&fit=crop');
 
 -- Insertar Proveedores Variados (Originales, Chinos y Personas Naturales)
 INSERT INTO proveedores (nombre, ruc_dni, direccion, telefono, email) VALUES
